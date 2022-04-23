689d2477875c795b740e20d739a4d9411a1163be
Irina <89231707647@mail.ru>
2022-04-23 16:39:57 +0700

    new commit

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 1: Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+int A = Prompt("Введите двойку = > ");
+int B = Prompt("Введите Ваше число на проверку = > ");
+int Stepen(int A)
+{
+    int i = 0;
+    int ans = 1;
+    for (A = 2; i < 10; i++)
+    {  
+        ans = ans * A;
+        Console.Write(ans + " ");
+        if (ans == B)
+        Console.Write("Вот оно ваше число - это степень двойки ");
+    }
+    Console.Write("Ваше число не является степенью двойки ");
+    return ans;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+Stepen(A);
---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 2: Проверка на простое число:
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+
+int A = Prompt("Введите ваше число на проверку = > ");
+
+void Stepen(int A)
+{
+      bool prost = true;
+    for (int i = 2; i <= A / 2; i++)
+    {
+        if (A % i == 0)
+        {
+            prost = false;
+            break;
+        }
+    }
+        if (prost)
+        {
+            Console.WriteLine("Ваше число слишком простое!!");
+        }
+        else
+        {
+            Console.WriteLine("Ваше число очень НЕ простое");
+        }
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+Stepen(A);
